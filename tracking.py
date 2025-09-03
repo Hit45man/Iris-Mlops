20eeb7708960cc4f9a91c8574c44149a38e678a8
Hit45man <krohit.kamble@gmail.com>
2025-09-03 13:16:16 +0530

    add mflow import

--- tracking.py
+++ tracking.py
@@ -4,6 +4,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from mlflow.tracking import MlflowClient
 import os
 
 # Set tracking uri to absolute path in current directory 
